94ae48f224270a7250fa2bb77c44d32784c19a88
DSieghard <86571071+DSieghard@users.noreply.github.com>
2023-04-10 19:48:57 -0400

    Minor code modifications

--- app/src/main/java/com/dsieghard/proyectochikito/MainActivity.kt
+++ app/src/main/java/com/dsieghard/proyectochikito/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.dsieghard.proyectochikito
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -19,23 +22,46 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
      binding = ActivityMainBinding.inflate(layoutInflater)
      setContentView(binding.root)
 
+
+        //Values Declaration for FAB Action
+        val intent = Intent(Intent.ACTION_SEND)
+        val email = getString(R.string.devmail)
+        val subject = getString(R.string.devsubject)
+        val text = getString(R.string.devtext)
+        val chooserIntent = Intent.createChooser(intent, getString(R.string.mailchooser))
+
+
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            //Get data and open mail app
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, email)
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, text)
+
+            //Validate if mail app is avaliable, and open default mail app
+            if(chooserIntent.resolveActivity(packageManager) != null) {
+                startActivity(chooserIntent)
+                Log.d("$this", "Opening default Mail App")
+            } else {
+                //Raise Error
+                Toast.makeText(this, getString(R.string.errorapp), Toast.LENGTH_SHORT).show()
+                Log.e("$this", "There is not a mail app installed")
+            }
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        //Sidebar items
         appBarConfiguration = AppBarConfiguration(setOf(
             R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -43,16 +69,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        //Inflate 3-dot button
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+            //Call SettingsActivity from 3-dot Button
             R.id.action_settings -> {
                 val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
+                Log.d("$this", "Opening Settings Activity")
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/dsieghard/proyectochikito/SettingsActivity.kt
+++ app/src/main/java/com/dsieghard/proyectochikito/SettingsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.title = getString(R.string.settings)
         supportFragmentManager
             .beginTransaction()
             .replace(android.R.id.content, SettingsFragment())
